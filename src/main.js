deba2fb984ccca4a01e55a18e25db1c680f06006
alex <ios@geekkun.com>
2019-08-04 15:30:02 +0300

    bootstrap added Buefy added, but discarded for now

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import Vue from 'vue'
+// import Buefy from 'buefy'
+// import 'buefy/dist/buefy.css'
+//
+// Vue.use(Buefy)
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 import router from './router'
 
